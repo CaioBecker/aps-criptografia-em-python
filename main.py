7ec6646fc00a9b46d3d3df60e88ed22c6d78d16e
Caio Becker <83302532+CaioBecker@users.noreply.github.com>
2022-10-23 14:10:32 -0300

    ajustes

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-def func_criptografar():
+def func_criptografar(cripto, texto):
+    texto_quebrado = []
+
+    texto_cripto = []
+
+    count = 0
+    while count < len(texto):
+        texto_quebrado.append(texto[count])
+        count = count + 1
 
 
+    for i in texto_quebrado:
+        print(i)
+        texto_cripto.append(cripto[i])
 
-cripto = {'1': 2, '2': 3, '3': 4, '4': 5, '5': 6, '6': 7, '7': 8, '8': 9, '9': 0, '0': 1}
+    print(texto)
+    texto = ''
 
-texto_quebrado = []
+    for i in texto_cripto:
+        texto = texto + str(i)
 
-texto_cripto = []
+    print(texto)
 
-texto = input("Digite um texto para criptografar:")
-count = 0
-while count < len(texto):
-    texto_quebrado.append(texto[a])
-    count = count + 1
+cripto_cm_1 = {'1': 2, '2': 3, '3': 4, '4': 5, '5': 6, '6': 7, '7': 8, '8': 9, '9': 0, '0': 1}
 
 
-for i in texto_quebrado:
-    print(i)
-    texto_cripto.append(cripto[i])
-
-print(texto)
-texto = ''
-
-for i in texto_cripto:
-    texto = texto + str(i)
-
-print(texto)
+func_criptografar(cripto_cm_1,input("Digite um texto para criptografar:"))
