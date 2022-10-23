2bb9b25979e04f14defcdd82b2012af1cc7512e1
Caio Becker <83302532+CaioBecker@users.noreply.github.com>
2022-10-23 17:56:39 -0300

    cifra de cesar feita com sucesso

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import chaves_cesar as cc
 
+
 def func_criptografar(cripto, texto):
     texto_quebrado = []
 
@@ -26,22 +27,14 @@ def func_criptografar(cripto, texto):
     return texto
 
 
-
-cripto_1 = func_criptografar(cc.cripto_cm_1, input("Digite um texto para criptografar:").upper())
-print(cripto_1)
-cripto_2 = func_criptografar(cc.cripto_cm_1, cripto_1)
-print(cripto_2)
-cripto_3 = func_criptografar(cc.cripto_cm_1, cripto_2)
-print(cripto_3)
-cripto_4 = func_criptografar(cc.cripto_cm_1, cripto_3)
+cripto_1 = func_criptografar(cc.cripto_cc, input("Digite um texto para criptografar:\n").upper())
+cripto_2 = func_criptografar(cc.cripto_cc, cripto_1)
+cripto_3 = func_criptografar(cc.cripto_cc, cripto_2)
+cripto_4 = func_criptografar(cc.cripto_cc, cripto_3)
 print(cripto_4)
 print('----------------')
-cripto_volta_1 = func_criptografar(cc.cripto_cm_1_volta, cripto_4)
+cripto_volta_1 = func_criptografar(cc.cripto_cc_volta, cripto_4)
 print(cripto_volta_1.upper())
 
-#1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+[{]};:|<,>.?
-
-
-
-
-
+#VALOR PARA TESTE
+#1234567890abcdefghijklmnopqrstuvwxyz !@#$%^&*()-_=+[{]};:|<,>.?
