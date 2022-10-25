8736b14c0c8956538533a4faf47b87ab98dbf563
Fernando Kuwahara <fernandokoliveira1405@gmail.com>
2022-10-24 22:33:16 -0300

    Concerto de bug
    Concerto de bug com laços de repetição, em relação à atribuição de valores no dicionário.

--- TransposicaoDeColunas.py
+++ TransposicaoDeColunas.py
@@ -1,17 +1,31 @@
 def CriptografarPorColunas(cripto_4):
     texto_separado = []
+    text_criptografado_pronto = []
     texto_criptografado = {}
 
-    count = 0
-    while count < len(cripto_4):
-        texto_separado.append(cripto_4[count])
-        count += 1
+    a = 0
+    while a < len(cripto_4):
+        texto_separado.append(cripto_4[a])
+        a += 1
 
-    i = 0
-    while i <= len(texto_separado):
-        teste = str(texto_separado[0:3])
-        texto_criptografado[teste] = i
+    b = 0
+    control = len(texto_separado) - 3
+    while b <= control:
+        texto_criptografado[str(texto_separado[0:3])] = b
         del (texto_separado[0:3])
-        i += 1
+        b += 1
+
+    for teste in texto_criptografado.keys():
+        if teste == "[]":
+            del (texto_criptografado[teste])
 
     print(texto_criptografado)
+
+    """text_criptografado_pronto.append(texto_criptografado)
+    texto_separado = []
+    for texto in text_criptografado_pronto:
+        for x, y in texto.items():
+            texto_separado.append(x + ": " + str(y))
+    """
+    print(texto_separado)
+    print(control)
