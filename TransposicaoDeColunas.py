fa3875f06720ef0a88643a31d77c3ca2e9515fa5
Caio Becker <83302532+CaioBecker@users.noreply.github.com>
2022-10-26 22:35:46 -0300

    ajustes

--- TransposicaoDeColunas.py
+++ TransposicaoDeColunas.py
@@ -1,9 +1,10 @@
-def func_criptografar_coluna(cripto,tp):
+import math
+
+
+def func_criptografar_coluna(cripto, tp):
     texto_separado = []
     text_criptografado_pronto = ''
     dc_arrays = {1: [], 2: [], 3: [], 4: []}
-
-    #COLOCAR TRY
 
     a = 0
     while a < len(cripto):
@@ -12,17 +13,24 @@ def func_criptografar_coluna(cripto,tp):
 
     a = 0
 
-    while a < round(len(cripto)/3):
+    while a < math.ceil((len(cripto) / 3)):
+
         dc_arrays[1].append(texto_separado[0])
         del (texto_separado[0])
-        dc_arrays[2].append(texto_separado[0])
-        del (texto_separado[0])
-        dc_arrays[3].append(texto_separado[0])
-        del (texto_separado[0])
+        try:
+            dc_arrays[2].append(texto_separado[0])
+            del (texto_separado[0])
+        except:
+            dc_arrays[2].append('~')
+        try:
+            dc_arrays[3].append(texto_separado[0])
+            del (texto_separado[0])
+        except:
+            dc_arrays[3].append('~')
 
         a += 1
 
-    if tp == 'D' :
+    if tp == 'D':
         dc_arrays[4] = dc_arrays[2]
         dc_arrays[2] = dc_arrays[1]
         dc_arrays[1] = dc_arrays[3]
@@ -35,15 +43,19 @@ def func_criptografar_coluna(cripto,tp):
 
     a = 0
 
-    while a < round(len(cripto) / 3):
+    while a < math.ceil((len(cripto) / 3)):
 
-        text_criptografado_pronto = text_criptografado_pronto + dc_arrays[1][a]
-        text_criptografado_pronto = text_criptografado_pronto + dc_arrays[2][a]
-        text_criptografado_pronto = text_criptografado_pronto + dc_arrays[3][a]
+        if dc_arrays[2][a] != '~':
+            text_criptografado_pronto = text_criptografado_pronto + dc_arrays[1][a]
+            text_criptografado_pronto = text_criptografado_pronto + dc_arrays[2][a]
+            text_criptografado_pronto = text_criptografado_pronto + dc_arrays[3][a]
+        else:
+            text_criptografado_pronto = text_criptografado_pronto + dc_arrays[3][a]
+            text_criptografado_pronto = text_criptografado_pronto + dc_arrays[1][a]
+            text_criptografado_pronto = text_criptografado_pronto + dc_arrays[2][a]
 
         a += 1
 
-
-    return text_criptografado_pronto
-    #for i in dc_arrays[1]:
+    return text_criptografado_pronto.replace('~', '')
+    # for i in dc_arrays[1]:
     #    print(i)
