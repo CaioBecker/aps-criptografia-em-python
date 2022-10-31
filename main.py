ed9eee2a5f306fb21cfe3f7f9279531927324af7
Caio Becker <83302532+CaioBecker@users.noreply.github.com>
2022-10-31 20:12:37 -0300

    .

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ while TRUE:
             
             padrao = int(input("Digite o padrão para a cifra de cesar:\n"))
             
-            cifra = cc.func_gerar_cifra(padrao)
+            cifra = cc.func_gerar_cifra(padrao, 'C')
             
             cripto_1 = cc.func_criptografar(cifra, input("Digite um texto para criptografar:\n"))
             padrao = padrao + padrao
             print(padrao)
-            cripto_2 = cc.func_criptografar(cifra, cripto_1)
+            """cripto_2 = cc.func_criptografar(cifra, cripto_1)
             padrao = padrao + padrao
             print(padrao)
             cripto_3 = cc.func_criptografar(cifra, cripto_2)
@@ -24,30 +24,27 @@ while TRUE:
             print(padrao)
             cripto_4 = cc.func_criptografar(cifra, cripto_3)
             padrao = padrao + padrao
-            print(padrao)
-            
-            print(f'Cifra de cesar:"{cripto_4}"')
+            print(padrao)"""
+            print(cifra)
+            print(f'Cifra de cesar: "{cripto_1}"')
 
-            cripto_5 = tdc.func_criptografar_coluna(cripto_4, 'C')
+            cripto_5 = tdc.func_criptografar_coluna(cripto_1, 'C')
 
-            print(f'Tranpocicao de colunas:"{cripto_5}"')
+            print(f'Tranpocicao de colunas: "{cripto_5}"')
         else:
             
-            padrao = (int(input("Digite o padrão para a cifra de cesar:\n")))*30
+            padrao = int(input("Digite o padrão para a cifra de cesar:\n"))
 
-            if padrao > 90:
-                padrao = padrao - 90
+            cifra = cc.func_gerar_cifra(padrao, 'D')
 
-            print(padrao)
-            cifra = cc.func_gerar_cifra(padrao)
+            cifra_des = cc.func_gerar_cifra_descripto(cifra)
 
-            print(cifra)
-            
+            print(cifra_des)
+
             des_cripto_1 = tdc.func_criptografar_coluna(input("Digite um texto para descriptografar:\n"), 'D')
+            des_cripto_2 = cc.func_criptografar(cifra_des, des_cripto_1)
 
-            des_cripto_2 = cc.func_criptografar(cifra, des_cripto_1)
-
-            print(f'Descriptografado:{des_cripto_2}')
+            print(f'Descriptografado: {des_cripto_2}')
 
         #VALOR PARA TESTE
         #1234567890abcdefghijklmnopqrstuvwxyz !@#$%^&*()-_=+[{]};:|<,>.?
